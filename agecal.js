5c35335b4570f8dadec30b05abf6f21fbd5e2590
Putul-Sarkar <putulsarkar8@gmail.com>
2023-09-13 10:46:03 +0600

    js added

--- agecal.js
+++ agecal.js
@@ -11,9 +11,9 @@ function calculateage() {
 
     let today = new Date();
 
-    let d2 = birthdate.getDate();
-    let m2 = birthdate.getMonth() + 1;
-    let y2 = birthdate.getFullYear();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
 
     let d3, m3, y3;
     y3 = y2 - y1;
@@ -37,7 +37,7 @@ function calculateage() {
         m3 = 11;
         y3--;
     }
-    result.innerHTML = "";
+    result.innerHTML = "You are ${m3}month, ${d3}date and ${y3}years old";
 }
 function getDaysInMonth(year, month) {
     return new Date(year, month, 0).getDate();
@@ -55,4 +55,3 @@ function getDaysInMonth(year, month) {
 
 
 
-}
